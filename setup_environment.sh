ed910ac6da526a41a03c8a5c318b12e7609988e5
deforum <110359942+deforum@users.noreply.github.com>
2023-04-16 19:48:16 -0700

    Create setup_environment.sh

---
+++ setup_environment.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Source .bashrc
+source ~/.bashrc
+
+# Create and activate conda environment
+conda create -n train python=3.10 -y
+conda activate train
+
+# Install dependencies
+conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia -y
+
+# Clone repositories
+git clone https://github.com/huggingface/diffusers.git
+git clone https://github.com/huggingface/transformers.git
+git clone https://github.com/huggingface/accelerate.git
+
+# Install xformers
+python -m pip install -U xformers
+
+# Install requirements from requirements.txt (assuming it's in the current directory)
+python -m pip install -r requirements.txt
+
+# Install accelerate, transformers, and diffusers
+cd accelerate
+python -m pip install -e .
+cd ../
+
+cd transformers
+python -m pip install -e .
+cd ../
+
+cd diffusers
+python -m pip install -e .
+cd ../
+
+echo "Setup completed!"
