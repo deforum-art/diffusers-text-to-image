cbec8f59a93641006a414242c85ca25e849d615f
deforum <110359942+deforum@users.noreply.github.com>
2023-04-16 19:49:57 -0700

    Create install_miniconda.sh

---
+++ install_miniconda.sh
@@ -0,0 +1,11 @@
+# Make the installer script executable
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+
+# Run the installer script
+echo "Installing Miniconda3..."
+bash Miniconda3-latest-Linux-x86_64.sh
+
+# Clean up the installer script
+rm Miniconda3-latest-Linux-x86_64.sh
+
+echo "Miniconda3 installation completed!"
